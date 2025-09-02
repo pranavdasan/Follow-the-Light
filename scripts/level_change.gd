1c6a135f7ca89a8781d6906de9a97f9bd532e79d
Pranav Dasan <pranavdasan8@gmail.com>
2025-09-01 22:40:08 -0700

    Added a web export

--- scripts/level_change.gd
+++ scripts/level_change.gd
@@ -3,10 +3,13 @@ extends Area2D
  
 
 func _on_body_entered(body):
+	print("Level change")
 	var current_scene_file = get_tree().current_scene.scene_file_path
 	var next_level_number = current_scene_file.to_int() + 1
 	
-	var next_level_path = "res://levels/level_" + str(next_level_number) + ".tcsn"
+	print(next_level_number)
 	
+	var next_level_path = "res://levels/level_" + str(next_level_number) + ".tscn"
 	
+	get_tree().change_scene_to_file(next_level_path)
 	
